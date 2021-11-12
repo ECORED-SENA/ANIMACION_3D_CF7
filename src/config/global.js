cf82b78f99a3bdf8ae5e0c05f9228801a94a6332
Edgar Mauricio Cortes <edgarm.cortes@misena.edu.co>
2021-11-12 07:47:41 -0500

    Ajustes

--- src/config/global.js
+++ src/config/global.js
@@ -63,6 +63,12 @@ export default {
     ],
     subMenu: [
       {
+        nombreRuta: 'actividad',
+        icono: 'far fa-question-circle',
+        titulo: 'Actividad didáctica',
+        desarrolloContenidos: true,
+      },
+      {
         nombreRuta: 'glosario',
         icono: 'fas fa-sort-alpha-down',
         titulo: 'Glosario',
